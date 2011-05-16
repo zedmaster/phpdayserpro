8836b838da9374cd751fe8bb7f54d64413817db7
Zed <alexandre@editoraquantum.com.br>
2011-05-16 18:38:51 -0300

    * Adicionando a tela das palestras

---
+++ application/views/helpers/Palestras.php
@@ -0,0 +1,33 @@
+<?php
+
+class App_View_Helper_Palestras extends Zend_View_Helper_Abstract
+{
+
+    public function palestras()
+    {
+        $html = "";
+
+        $table = new Application_Model_DbTable_Minicurso();
+        $db = $table->getAdapter();
+        $stmt = $db->query('SELECT * 
+				            FROM palestra a INNER JOIN palestra_palestrante b USING(id_palestra)
+             				INNER JOIN user c USING(id_user) ;');
+        $rows = $stmt->fetchAll(PDO::FETCH_CLASS);
+        
+
+        foreach($rows as $val)
+        {
+            $data = new Zend_Date($val->data_hora);
+
+            $html .= "<h3>".$val->nome_palestra."</h3>";
+            $html .= "  <ul>";
+            $html .= "      <li>Horário: ".$data->get('dd/MM/YYYY hh:mm')."h</li>";
+            $html .= "      <li>Duração: 50min</li>";
+            $html .= "      <li>Palestrante: ".$val->nome."</li>";
+            $html .= "      <li>Descrição:<p>".$val->descricao."</p></li>";
+            $html .= "  </ul>";
+        }
+
+        return $html;
+    }
+}
