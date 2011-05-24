96d8c799071902c5ac89b850946fa9d47b38f2cc
Alexandre F. Martins <zedmaster@gmail.com>
2011-05-24 00:38:59 -0300

    * Correção do link da imagem do tutor.

--- application/views/helpers/Tutores.php
+++ application/views/helpers/Tutores.php
@@ -20,7 +20,7 @@ class App_View_Helper_Tutores extends Zend_View_Helper_Abstract
         {
             $html .= "<h3 id=\"{$val->id_user}\">".$val->nome."</h3>";
             $html .= "  <ul>";
-            $html .= "      <dd><img src\"/img/user/{$val->id_user}.jpg\"></dd>";
+            $html .= "      <dd><img src=\"/img/user/{$val->id_user}.jpg\"></dd>";
             $html .= "      <dd><strong>Blog/Site:</strong> <a href=\"{$val->site}\" target=\"new\">{$val->site}</a></dd>";
             $html .= "      <dd><strong>Twitter:</strong> <a href=\"{$val->twitter}\" target=\"new\">{$val->twitter}</a></dd>";
             $html .= "      <dd><strong>Sobre:</strong> <p>".str_replace("\r","<br/>",$val->curriculo)."</p></dd>";
